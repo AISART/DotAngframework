0f00d442549af974ae1e9a7279f844b144c351f1
Arie Blonk <ablonk@dynasource.com>
2018-11-20 12:43:51 +0100

    Added registration

--- DotAng.API/Controllers/ValuesController.cs
+++ DotAng.API/Controllers/ValuesController.cs
@@ -22,6 +22,7 @@ namespace DotAng.SPA.Controllers
 
         }
         // GET api/values
+        [AllowAnonymous]
         [HttpGet]
         public async Task<IActionResult> GetValues()
         {
