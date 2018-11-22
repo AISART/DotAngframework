6e1351f2562932557e5943cfe9314fe868b9a12a
Arie Blonk <ablonk@dynasource.com>
2018-11-22 12:09:11 +0100

    Added 3rd party librarys

--- DotAng.API/Controllers/AuthController.cs
+++ DotAng.API/Controllers/AuthController.cs
@@ -48,8 +48,6 @@ namespace DotAng.API.Controllers
         [HttpPost("login")]
         public async Task<IActionResult> Login(UserForLoginDto userForLoginDto)
         {
-            throw new Exception("Computer says no!");
-
             var userFromRepo = await _repo.Login(userForLoginDto.Username.ToLower(), userForLoginDto.Password);
 
             if(userFromRepo == null)
